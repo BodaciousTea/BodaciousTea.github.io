2592f51bfbf593469d338fe62d80dbff3e1bd171
BodaciousTea <tebodacious@gmail.com>
2023-09-09 00:57:16 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// Check if it's a production build
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  // Output as static export
+  output: 'export',
+
+  // Add the asset prefix for production builds on GitHub Pages
+  assetPrefix: isProd ? '/tedkoller' : '',
+
+  // Set the base path for GitHub Pages
+  basePath: isProd ? '/tedkoller' : '',
+}
+
+module.exports = nextConfig;
