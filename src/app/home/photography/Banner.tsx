b3c68fe06c374546b165e27fce35c5955189df4e
BodaciousTea <tebodacious@gmail.com>
2023-11-17 17:47:37 -0600

    Site Revamp

--- src/app/home/photography/Banner.tsx
+++ src/app/home/photography/Banner.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import React, {useEffect, useState} from "react";
-import Card3D from "@/components/common/card_3d/Card3D";
-import TextReveal from "@/components/common/text_reveal/TextReveal";
+import React from 'react';
+import Card3D from '@/components/common/card_3d/Card3D';
+import TextReveal from '@/components/common/text_reveal/TextReveal';
 
 interface BannerProps {}
 
@@ -16,18 +16,19 @@ function Banner(props: BannerProps) {
             </TextReveal>
           </h2>
         </div>
-        <div
-          id="photography"
-          className="flex flex-col justify-center px-[10vw]"
-        >
+        <div id="photography" className="flex flex-col justify-center px-[10vw]">
           <div className="w-full max-w-[70vw] mx-auto">
-            <img
-              className="w-full"
-              src="/images/Eva Walker Senior 1.jpg"
-              alt=""
-            />
+            <img className="w-full" src="/images/Eva Walker Senior 1.jpg" alt="" />
           </div>
         </div>
+        <a
+          href="https://www.tedkoller.com/gallery"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="uppercase text-md leading-none font-normal border-[0.117vw] button-style absolute bottom-[5.2vw] right-12 -translate-x-1/2 cursor-pointer"
+        >
+          VIEW ALL
+        </a>
       </div>
     </Card3D>
   );
--- src/app/home/watch/WatchHere.tsx
+++ src/app/home/watch/WatchHere.tsx
@@ -1,14 +1,7 @@
-/* eslint-disable @next/next/no-img-element */
 import Card3D from "@/components/common/card_3d/Card3D";
-import React, { useState } from "react";
+import React from "react";
 
 function WatchHere() {
-  const [hovered, setHovered] = useState(false);
-
-  const hoverStyles = hovered
-    ? { textDecoration: 'underline', textDecorationThickness: '1px' }
-    : {};
-
   return (
     <Card3D>
       <div className="ml-[14.5vw] w-[56vw] relative">
@@ -17,10 +10,7 @@ function WatchHere() {
           href="https://www.youtube.com/watch?v=pQLBevvG9u4"
           target="_blank"
           rel="noopener noreferrer"
-          className="uppercase text-md leading-none font-normal border-[0.117vw] border-white px-[0.82vw] absolute bottom-[5.2vw] right-0 translate-x-1/2 cursor-pointer"
-          style={hoverStyles}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
+          className="uppercase text-md leading-none font-normal border-[0.117vw] button-style absolute bottom-[5.2vw] right-0 translate-x-1/2 cursor-pointer"
         >
           WATCH HERE
         </a>
