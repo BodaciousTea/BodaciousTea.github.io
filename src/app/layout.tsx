1524954e16314944a4a42e7d0baa6344fe1dfc6f
BodaciousTea <tebodacious@gmail.com>
2024-01-22 21:22:16 -0600

    seo1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,41 @@
 import "../styles/main.css";
 import "animate.css";
-import type {Metadata} from "next";
+import Head from 'next/head';
 
-export const metadata: Metadata = {
-  title: "TED KOLLER",
-  description: "Ted Koller, Multimedia Professional | Photographer | Web Designer | Videographer | Drone Pilot and more...",
-};
+interface RootLayoutProps {
+  children: React.ReactNode; // For the children prop
+  pageTitle?: string;        // Optional string for pageTitle
+  pageDescription?: string;  // Optional string for pageDescription
+}
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children, pageTitle, pageDescription }) {
+  const ogImagePath = '/images/open-graph-image.jpg';
+
+  const title = pageTitle || "Home | Ted Koller Portfolio";
+  const description = pageDescription || "Ted Koller's professional portfolio | Includes projects in photography, web development, videography, design, compositing, and more...";
+
   return (
     <html lang="en">
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content="Photography, Web Design, Videography, " />
+        <meta name="author" content="Ted Koller" />
+        <link rel="icon" sizes="32x32" href="/images/favicon%2032x32.png" type="image/png" />
+        <link rel="icon" sizes="192x192" href="/images/favicon%20192x192.png" type="image/png" />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={ogImagePath} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+
+      </Head>
       <body>{children}</body>
     </html>
   );
