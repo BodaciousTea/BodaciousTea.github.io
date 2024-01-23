4ea8d3cf08bd80da36ed4d7daf8e98a902040de5
BodaciousTea <tebodacious@gmail.com>
2024-01-23 11:48:55 -0600

    description tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,6 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
       <meta property="og:title" content="Ted Koller's Portfolio" />
       <meta property="og:description" content="Ted Koller's professional portfolio | Includes projects in photography, web development, videography, design, compositing, and more..." />
       <meta property="og:image" content="/images/open-graph-image.jpg" />
-      <meta name="description" content="A brief description of the page content." />
-      <meta name="description" content="Ted Koller, Multimedia Professional | Photographer | Web Designer | Videographer | Drone Pilot and more..." />
       <meta name="keywords" content="Photography, Web Design, Videography, Kansas City, KC, Ted Koller, Photo, Video, Website" />
       <meta name="author" content="Ted Koller" />
     </html>
