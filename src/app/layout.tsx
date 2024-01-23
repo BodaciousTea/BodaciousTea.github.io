b0aeedf2f1c2f1715f429f1e3a68a26e98f75e10
BodaciousTea <tebodacious@gmail.com>
2024-01-22 21:48:09 -0600

    redo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,15 @@
 import "../styles/main.css";
 import "animate.css";
-import Head from 'next/head';
+import type {Metadata} from "next";
 
-interface RootLayoutProps {
-  children: React.ReactNode;
-  pageTitle?: string;
-  pageDescription?: string;
-}
+export const metadata: Metadata = {
+  title: "TED KOLLER",
+  description: "Ted Koller, Multimedia Professional | Photographer | Web Designer | Videographer | Drone Pilot and more...",
+};
 
-export default function RootLayout({ children, pageTitle, pageDescription }: RootLayoutProps) {
-  const ogImagePath = '/images/open-graph-image.jpg';
-  const title = pageTitle || "Home | Ted Koller Portfolio";
-  const description = pageDescription || "Ted Koller's professional portfolio | Includes projects in photography, web development, videography, design, compositing, and more...";
-
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content="Photography, Web Design, Videography" />
-        <meta name="author" content="Ted Koller" />
-        <link rel="icon" sizes="32x32" href="/images/favicon%2032x32.png" type="image/png" />
-        <link rel="icon" sizes="192x192" href="/images/favicon%20192x192.png" type="image/png" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={ogImagePath} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:type" content="website" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-      </Head>
       <body>{children}</body>
     </html>
   );
