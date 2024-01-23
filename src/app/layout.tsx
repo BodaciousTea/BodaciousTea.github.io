5a8fcc6f2dbaa2e05e5fc6d6bc74621756afa73f
BodaciousTea <tebodacious@gmail.com>
2024-01-22 21:47:14 -0600

    seo8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
+import "../styles/main.css";
+import "animate.css";
 import Head from 'next/head';
-import React from 'react';
 
-// Define an interface for the props
 interface RootLayoutProps {
-  children: React.ReactNode; // For the children prop
-  pageTitle?: string;        // Optional string for pageTitle
-  pageDescription?: string;  // Optional string for pageDescription
+  children: React.ReactNode;
+  pageTitle?: string;
+  pageDescription?: string;
 }
 
 export default function RootLayout({ children, pageTitle, pageDescription }: RootLayoutProps) {
   const ogImagePath = '/images/open-graph-image.jpg';
-
   const title = pageTitle || "Home | Ted Koller Portfolio";
   const description = pageDescription || "Ted Koller's professional portfolio | Includes projects in photography, web development, videography, design, compositing, and more...";
 
   return (
-    <>
+    <html lang="en">
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
@@ -23,19 +22,17 @@ export default function RootLayout({ children, pageTitle, pageDescription }: Roo
         <meta name="author" content="Ted Koller" />
         <link rel="icon" sizes="32x32" href="/images/favicon%2032x32.png" type="image/png" />
         <link rel="icon" sizes="192x192" href="/images/favicon%20192x192.png" type="image/png" />
-
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={ogImagePath} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:type" content="website" />
-
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
       </Head>
-      <main>{children}</main>
-    </>
+      <body>{children}</body>
+    </html>
   );
 }
