f1314872e603fbbe96460bdcf3b8921d20a01ebb
Owen000 <rice@rice>
2024-01-26 23:20:10 -0600

    stuff

--- src/app/home/About.tsx
+++ src/app/home/About.tsx
@@ -12,13 +12,19 @@ function About() {
         <div className="mountain-mask"></div>
 
         <div className="text-center absolute top-20 left-1/2 transform -translate-x-1/2 z-10">
-          <h2 className="text-masthead font-bold text-4xl lg:text-5xl">
-            <TextReveal><span>ABOUT ME</span></TextReveal>
+          <h2 className="about-text font-bold">
+            <TextReveal><span>ABOUT</span></TextReveal>
+          </h2>
+          <h2 className="ted-text font-bold">
+            <TextReveal><span>TED</span></TextReveal>
           </h2>
         </div>
 
-        <div className="text-center relative z-30 mt-[55vh]">
-          <p className="text-custom font-medium mb-10 mx-auto max-w-[60ch] md:max-w-[40ch] lg:max-w-[50ch]">
+        <div className="text-center relative z-30 mt-[30vh]"> {/* Adjusted margin-top */}
+          <p 
+            style={{ fontSize: '22px', lineHeight: '1.2', paddingLeft: '20px', paddingRight: '20px' }}
+            className="font-medium mb-10 mx-auto max-w-[60ch] md:max-w-[40ch] lg:max-w-[50ch]"
+          >
             I am passionate about what I do as I <br />
             pursue skills in technology, photography, <br />
             videography, drone operation, web-<br />
--- src/app/home/photography/Banner.tsx
+++ src/app/home/photography/Banner.tsx
@@ -4,20 +4,17 @@ import Carousel from '@/components/common/Carousel';
 function Banner() {
   const carouselItems = [
     {
-      title: 'PHOTOGRAPHY',
-      imageUrl: '/images/Eva Walker Senior 1.jpg',
+      imageUrl: '/Mobile/images/carouselMobile1.png',
       titleStyle: { background: 'linear-gradient(to right, #43e97b 0%, #38f9d7 100%)' },
       onButtonClick: () => window.open('https://www.tedkoller.com/gallery', '_blank')
     },
     {
-      title: 'WEB DESIGN',
-      imageUrl: '/images/webdesign1.jpg',
+      imageUrl: '/Mobile/images/carouselMobile2.png',
       titleStyle: { background: 'linear-gradient(to right, #ff758c 0%, #ff7eb3 100%)' },
       onButtonClick: () => {/* handle click for Web Design */}
     },
     {
-      title: 'VIDEO PRODUCTION',
-      imageUrl: '/images/videoprod1.jpg',
+      imageUrl: '/Mobile/images/carouselMobile3.png',
       titleStyle: { background: 'linear-gradient(to right, #667eea 0%, #764ba2 100%)' },
       onButtonClick: () => {/* handle click for Video Production */}
     }
