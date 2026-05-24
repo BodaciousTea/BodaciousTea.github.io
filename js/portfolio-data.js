2cbe123dd77eeb773500b1e8f7a84fdb34bdffec
BodaciousTea <tebodacious@gmail.com>
2026-05-23 19:17:10 -0500

    fixed js

---
+++ js/portfolio-data.js
@@ -0,0 +1,135 @@
+const portfolioData = [
+  {
+    id: 1,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-19.webp",
+    images: [
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-21.jpg",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-23.webp",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-29.webp",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/elviaje_2--05.webp",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Ojinaga-MX.webp"
+    ],
+    title: "El Viaje",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 2,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Tour-future%26metroBoomin.webp",
+    images: [
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Tour-future%26metroBoomin%20-%20Full.webp",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/future%26metroBoomin-8.webp"
+    ],
+    title: "Future & Metro Boomin Tour",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 3,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/travis-scott-tour-02.webp",
+    images: [
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/travis-scott-tour-05.webp"
+    ],
+    title: "Travis Scott Tour",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 4,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Nacreous%20Coffee%20Composite_Full.webp",
+    images: [
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Section%205%20Frame%202%20-%20CloudsCoffeeChristmas.webp",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Section%205%20Frame%204%20-%20CloudsCoffeePeppermint.webp"
+    ],
+    title: "Nacreous Coffee",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 5,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/kawmixThumbnail3.mp4",
+    images: [],
+    title: "Kawmix",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 6,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coverCropThumbnail1.mp4",
+    images: [],
+    title: "Cover Crop",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 7,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/green_pastures_farm_preview.mp4",
+    images: [],
+    title: "Green Pastures Farm",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 8,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/theCoupes.mp4",
+    images: [],
+    title: "The Coupes",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 9,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/underground-096.webp",
+    images: [
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/child-of-hope%20ACT2-13.webp"
+    ],
+    title: "Underground",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 10,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Iowa_Full.webp",
+    images: [],
+    title: "Iowa",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 11,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Publiceye.png",
+    images: [],
+    title: "Public Eye",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 12,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/SNY02887.jpg",
+    images: [],
+    title: "SNY02887",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 13,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Wildlife1.webp",
+    images: [],
+    title: "Wildlife",
+    date: "",
+    description: "",
+    links: []
+  }
+];
