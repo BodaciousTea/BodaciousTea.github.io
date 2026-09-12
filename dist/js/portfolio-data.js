0c523abb5b728a87f5eb27bf95d680443b632e95
BodaciousTea <tebodacious@gmail.com>
2026-09-12 13:35:02 -0500

    Keep gallery to current media sets

--- dist/js/portfolio-data.js
+++ dist/js/portfolio-data.js
@@ -1,161 +1,6 @@
 const portfolioData = [
   {
     id: 1,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-19.webp",
-    images: [
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-21.jpg",
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-23.webp",
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/el-viaje-29.webp",
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/elviaje_2--05.webp",
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Ojinaga-MX.webp"
-    ],
-    title: "MX / TX",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 2,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Tour-future%26metroBoomin.webp",
-    images: [
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Tour-future%26metroBoomin%20-%20Full.webp",
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/future%26metroBoomin-8.webp"
-    ],
-    title: "Future & Metro Boomin Tour",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 3,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/travis-scott-tour-02.webp",
-    images: [
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/travis-scott-tour-05.webp"
-    ],
-    title: "Travis Scott Tour",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 4,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/clouds_coffee_l.webp",
-    images: [
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/clouds_coffee_c.webp",
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/clouds_coffee_r.webp"
-    ],
-    title: "Clouds Coffee",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 5,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/kawmixThumbnail3.mp4",
-    images: [],
-    title: "Kawmix",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 6,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coverCropThumbnail1.mp4",
-    images: [],
-    title: "Cover Crop",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 7,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/green_pastures_farm_preview.mp4",
-    images: [],
-    title: "Green Pastures Farm",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 8,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coupes_L.mp4",
-    images: [
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coupes_c.webp",
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coupes_r.webp"
-    ],
-    title: "The Coupes",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 9,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/underground-096.webp",
-    images: [
-      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/child-of-hope%20ACT2-13.webp"
-    ],
-    title: "Underground",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 10,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Iowa_Full.webp",
-    images: [],
-    title: "Iowa",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 11,
-    category: "web",
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Publiceye.png",
-    images: [],
-    title: "Public Eye",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 12,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/grand_tetons_compressed.jpg",
-    images: [],
-    title: "Grand Tetons",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 13,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/Wildlife1.webp",
-    images: [],
-    title: "Wildlife",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 14,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/tomatoes_jonah_nelson_preview.mp4",
-    images: [],
-    title: "Family Tree Nursery",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 15,
-    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/internal_training_jonah_nelson_preview.mp4",
-    images: [],
-    title: "Internal Corporate Training",
-    date: "",
-    description: "",
-    links: []
-  },
-  {
-    id: 16,
     thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/circus_maximus_tour_l.webp",
     images: [
       "https://raw.githubusercontent.com/BodaciousTea/public-images/main/circus_maximus_tour_c.webp",
@@ -167,7 +12,31 @@ const portfolioData = [
     links: []
   },
   {
-    id: 17,
+    id: 2,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/clouds_coffee_l.webp",
+    images: [
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/clouds_coffee_c.webp",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/clouds_coffee_r.webp"
+    ],
+    title: "Clouds Coffee",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 3,
+    thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coupes_L.mp4",
+    images: [
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coupes_c.webp",
+      "https://raw.githubusercontent.com/BodaciousTea/public-images/main/coupes_r.webp"
+    ],
+    title: "The Coupes",
+    date: "",
+    description: "",
+    links: []
+  },
+  {
+    id: 4,
     thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/culturehouse_l.webp",
     images: [
       "https://raw.githubusercontent.com/BodaciousTea/public-images/main/culturehouse_c.webp",
@@ -179,7 +48,7 @@ const portfolioData = [
     links: []
   },
   {
-    id: 18,
+    id: 5,
     thumbnail: "https://raw.githubusercontent.com/BodaciousTea/public-images/main/we_trust_you_tour_l.mp4",
     images: [
       "https://raw.githubusercontent.com/BodaciousTea/public-images/main/we_trust_you_tour_c.webp",
